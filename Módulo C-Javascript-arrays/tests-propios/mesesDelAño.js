8e0ef1f3ffc6daa18a9efc9670c5d5e0d2d3605e
miltonzini <miltonzini@gmail.com>
2023-03-08 14:08:58 -0300

    fix mesesDelAños
    2nd approach

--- Módulo C-Javascript-arrays/tests-propios/mesesDelAño.js
+++ Módulo C-Javascript-arrays/tests-propios/mesesDelAño.js
@@ -4,29 +4,21 @@
 // Si alguno de los meses no está, retornar el string: "No se encontraron los meses pedidos".
 // Tu código:
 
-// Meses del Año: enfoque 1
-
-var array = ["Enero", "Febrero", "Marzo", "Noviembre", "Diciembre"]; // test
+// Meses del Año: enfoque 2
+var array = ["Enero", "Febrero", "Marzo", "Noviembre", "Diciembre"];
 function mesesDelAño(array) {
+   var newArray = [];
    if (array.includes("Enero") && array.includes("Marzo") && array.includes("Noviembre")) {
-      var newArray = array.map(function (mes) {
-         if (mes === "Enero" || mes === "Marzo" || mes === "Noviembre") {
-            console.log(mes + " está en la lista")
-            return mes;
-         } else {
-            console.log(mes + " no está en la lista");
+      for (let i = 0; i < array.length; i++) {
+         if (array[i] === "Enero" || array[i] === "Marzo" || array[i] === "Noviembre") {
+            newArray.push(array[i]);
          };
-      });
-
+      };
       console.log(newArray);
-      // TO DO: quitar los 'undefined' de newArray antes de retornar.
-      // debería hacer otro for loop eliminándolos. Poco eficiente. 
-      // probar otro approach
       return newArray;
    } else {
       console.log("No se encontraron los meses pedidos");
       return "No se encontraron los meses pedidos";
    };
-   
-}
+};
 mesesDelAño(array);
