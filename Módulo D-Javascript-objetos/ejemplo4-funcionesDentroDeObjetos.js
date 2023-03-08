bcdfcb0cb6bc42a9ca8f6e03ca2d3b6bbd662900
miltonzini <miltonzini@gmail.com>
2023-03-08 19:55:47 -0300

    Create ejemplo4-funcionesDentroDeObjetos.js

---
+++ Módulo D-Javascript-objetos/ejemplo4-funcionesDentroDeObjetos.js
@@ -0,0 +1,14 @@
+var misFunciones = {
+    saludar: function() {
+        console.log('Hola')
+    },
+    dosMasDos: function() {
+        console.log(2+2)
+    },
+    multipliarDosNumeros: function(x, y) {
+        console.log(x*y)
+    },
+};
+misFunciones.saludar();
+misFunciones.dosMasDos();
+misFunciones.multipliarDosNumeros(5, 5);
