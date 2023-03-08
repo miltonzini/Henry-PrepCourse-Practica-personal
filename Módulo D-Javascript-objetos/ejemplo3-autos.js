3633cbb3ea46e1c89f3628069380a90a37667d34
miltonzini <miltonzini@gmail.com>
2023-03-08 19:54:00 -0300

    Create ejemplo3-autos.js

---
+++ Módulo D-Javascript-objetos/ejemplo3-autos.js
@@ -0,0 +1,7 @@
+var autos = {};
+autos.marcas = ['Ford', 'Audi', 'Ferrari']; // crear propiedad
+autos.colores = ['Rojo', 'Negro', 'Azul'];
+console.log(autos);
+
+delete autos.colores; // eliminar propiedad
+console.log(autos);
