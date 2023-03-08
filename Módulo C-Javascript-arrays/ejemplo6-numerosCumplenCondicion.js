74fb48cb5cf3630e26c865a3654e47c6d76f317e
miltonzini <miltonzini@gmail.com>
2023-03-08 14:57:18 -0300

    Create ejemplo6-numerosCumplenCondicion.js

---
+++ Módulo C-Javascript-arrays/ejemplo6-numerosCumplenCondicion.js
@@ -0,0 +1,10 @@
+// Ejemplo 6
+// imprimir números que cumplan la condición (ser 3);
+var numeros = [1, 2, 3, 4];
+numeros.forEach((num) => {
+    if(num === 3){
+        console.log(num);
+    }
+});
+// probar luego hacer una variación en la que, dado un arreglo con diferentes valores, me devuelva solo los números 
+// (en caso de que los hubiera)
