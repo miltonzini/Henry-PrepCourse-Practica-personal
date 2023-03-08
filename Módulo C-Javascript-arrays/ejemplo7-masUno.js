4cb6ce230c9ddb0c9af57d3f7ceb0aeb8f43f020
miltonzini <miltonzini@gmail.com>
2023-03-08 14:57:23 -0300

    Create ejemplo7-masUno.js

---
+++ Módulo C-Javascript-arrays/ejemplo7-masUno.js
@@ -0,0 +1,7 @@
+// Ejemplo 7
+// dado un array de números, devolver otro en el que cada valor sea incrementado en uno.
+var numeros = [1, 3, 9, 12];
+var masUno = numeros.map((num) => {
+   return num + 1;
+});
+console.log(masUno);
