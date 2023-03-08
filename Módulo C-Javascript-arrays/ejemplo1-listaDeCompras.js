d81c5687810edafc3299a2673cea2e38413994bc
miltonzini <miltonzini@gmail.com>
2023-03-08 14:48:16 -0300

    create listaDeCompras and listaDeColores

---
+++ Módulo C-Javascript-arrays/ejemplo1-listaDeCompras.js
@@ -0,0 +1,12 @@
+// Ejemplo 1
+// agregar elementos a un array en diferentes índices, retornarlo y retornar la cantidad de elemntos (incluídos los vacíos)
+var listaDeCompras = [];
+listaDeCompras[1] = 'lechuga'; 
+listaDeCompras[3] = 'tomates';
+
+console.log(listaDeCompras);
+
+var elementoDelArray = listaDeCompras[1];
+console.log(elementoDelArray);
+
+console.log("'listaDeCompras' tiene " + (listaDeCompras.length) +  " elementos.");
---
+++ Módulo C-Javascript-arrays/ejemplo2-listaDeColores.js
@@ -0,0 +1,9 @@
+// Ejemplo 2
+var listaDeColores = ['amarillo', 'azul'];
+listaDeColores.push('rojo'); // agrega elemento al final
+listaDeColores.unshift('verde'); // agrega elemento al principio
+console.log(listaDeColores);
+listaDeColores.pop(); // elimina el último elemento
+listaDeColores.shift(); // elimina el primer elemento
+console.log(listaDeColores);
+console.log(listaDeColores.length); // cantidad de elementos
