59548b27276935e1512b0999ba72cea1424146bf
miltonzini <miltonzini@gmail.com>
2023-03-08 19:38:29 -0300

    Create bucles.js

---
+++ Módulo C-Javascript-arrays/bucles.js
@@ -0,0 +1,19 @@
+// Imprimir los valores de la variable de iteración en un for loop. En este csa, siendo i < 5
+console.log('## For Loop');
+var suma = 0;
+for (var i = 0 ; i < 5; i++) {
+    suma = suma + i;
+    console.log('variable de iteración: ', i);
+}
+console.log('Variable Suma: ', suma);
+
+
+
+// usando un while loop imprimir números del 1 al 10 (incrementando imprimiendo en cada iteración)
+console.log(' ');
+console.log('## While loop');
+suma = 0;
+while (suma < 10) {
+    suma = suma + 1;
+    console.log(suma);
+}
