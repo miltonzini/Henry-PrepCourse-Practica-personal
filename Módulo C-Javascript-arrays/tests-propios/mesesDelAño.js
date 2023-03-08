f93d0c63aa8a3161d12c01bba4e0fb13a7b20535
miltonzini <miltonzini@gmail.com>
2023-03-08 14:05:23 -0300

    practice JS snippet
    first approach.

---
+++ Módulo C-Javascript-arrays/tests-propios/mesesDelAño.js
@@ -0,0 +1,30 @@
+// Consigna Original
+// El arreglo contiene algunos meses del año desordenados. Debes recorrerlo, buscar los meses "Enero",
+// "Marzo" y "Noviembre", guardarlos en un nuevo arreglo y retornarlo.
+// Si alguno de los meses no está, retornar el string: "No se encontraron los meses pedidos".
+// Tu código:
+
+// Meses del Año: enfoque 1
+
+var array = ["Enero", "Febrero", "Marzo", "Noviembre", "Diciembre"]; // test
+function mesesDelAño(array) {
+   if (array.includes("Enero") && array.includes("Marzo") && array.includes("Noviembre")) {
+      var newArray = array.map(function (mes) {
+         if (mes === "Enero" || mes === "Marzo" || mes === "Noviembre") {
+            console.log(mes + " está en la lista")
+            return mes;
+         } else {
+            console.log(mes + " no está en la lista");
+         };
+      });
+
+      console.log(newArray);
+      // TO DO: quitar los 'undefined' de newArray antes de retornar.
+      return newArray;
+   } else {
+      console.log("No se encontraron los meses pedidos");
+      return "No se encontraron los meses pedidos";
+   };
+   
+}
+mesesDelAño(array);
