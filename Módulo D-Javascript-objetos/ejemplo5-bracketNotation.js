740ff1d0024219caef9f63401cca55d18107b3f0
miltonzini <miltonzini@gmail.com>
2023-03-10 01:47:26 -0300

    Create ejemplo5-bracketNotation.js

---
+++ Módulo D-Javascript-objetos/ejemplo5-bracketNotation.js
@@ -0,0 +1,9 @@
+// Bracket Notation
+var atuendos = {
+    Manos: ['Guantes', 'Anillos'],
+    Pies: ['Zapatos', 'Medias'],
+}
+// console.log(atuendos);
+atuendos['Piernas'] = ['Bermudas', 'Pantalones'];
+
+console.log(atuendos);
