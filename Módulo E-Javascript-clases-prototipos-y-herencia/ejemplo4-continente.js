c45cc126e651b096d767308df867fcdb0e0f12b7
miltonzini <miltonzini@gmail.com>
2023-03-11 15:54:15 -0300

    reformular ejemplo4-continente.js
    reformulación propia (menos ambigua) del ejercicio.

--- Módulo E-Javascript-clases-prototipos-y-herencia/ejemplo4-continente.js
+++ Módulo E-Javascript-clases-prototipos-y-herencia/ejemplo4-continente.js
@@ -1,12 +1,24 @@
-class LatinoAmerica {
-	constructor() {
-		this.paises = [ ];
-	};
-};
- 
-LatinoAmerica.prototype.agregarPais = function (pais) {
-   	this.paises.push(pais);
-};
-let continente = new LatinoAmerica();
-continente.agregarPais('México');
-console.log(continente.paises);
+// Creamos un método que agrega un país al array países (propiedad de la clase Continente) 
+class Continente {
+    constructor() {
+        this.paises = [];
+    }
+}
+
+Continente.prototype.agregarPais = function(pais) {
+    this.paises.push(pais);
+}; // inventé el método "agregarPais" y ahora puedo usarlo en cualquier objeto de la clase "Continente".
+
+var latinoamerica = new Continente();
+latinoamerica.agregarPais('Mexico');
+latinoamerica.agregarPais('Perú');
+latinoamerica.agregarPais('Argentina');
+latinoamerica.agregarPais('Chile');
+console.log(latinoamerica.paises);
+
+var europa = new Continente();
+europa.agregarPais('Francia');
+europa.agregarPais('Italia');
+europa.agregarPais('Reino Unido');
+europa.agregarPais('Alemania');
+console.log(europa.paises);
