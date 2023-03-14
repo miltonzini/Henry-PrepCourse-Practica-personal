220ab00b81675b0c1126f475a11596470579de57
miltonzini <miltonzini@gmail.com>
2023-03-13 21:47:07 -0300

    Create devolverArraySoloNumeros.js

---
+++ Módulo C-Javascript-arrays/tests-propios/devolverArraySoloNumeros.js
@@ -0,0 +1,11 @@
+// Otra variación del ejemplo 6
+// Dado un array con datos mixtos devolver nuevo array solo con datos numéricos.
+
+var arrayMixto = [1, 2, 3, 4, "palabra", "asd", 5];
+var nuevoArray = []
+for (var i = 0; i < arrayMixto.length; i++) {
+    if (typeof(arrayMixto[i]) === "number") nuevoArray.push(arrayMixto[i]);
+}
+
+
+console.log(nuevoArray);
