5347b3f3bd0224e94ab2f7abbc7c216ad1ad24e1
miltonzini <miltonzini@gmail.com>
2023-03-18 23:11:57 -0300

    Update test-crearClasePersona.js

--- Módulo Final - SimulacroChallenge/tests-propios/test-crearClasePersona.js
+++ Módulo Final - SimulacroChallenge/tests-propios/test-crearClasePersona.js
@@ -2,14 +2,10 @@
 
 class Persona {
     constructor(nombre, edad, hobbies, amigos) {
-    // El constructor de la clase Persona recibe nombre (string), edad (integer), hobbies (array de strings), amigos (array de objetos)
-    // Inicializar las propiedades de la persona con los valores recibidos como argumento
-
     this.nombre = nombre;
     this.edad = edad;
     this.hobbies = hobbies;
     this.amigos = amigos;
-
     }
 
     addFriend(nombre, edad) {
@@ -31,31 +27,17 @@ class Persona {
     }
 
     getHobbies() {
-    // El método 'getHobbies' debe retornar un arreglo con los hobbies de la persona
-    // Ej:
-    // persona.getHobbies() debe devolver ['correr', 'dormir', 'nadar']
-
-    // Tu código aca:
-
+        console.log(this.hobbies);
     }
 
     getPromedioEdad() {
-    // El método 'getPromedioEdad' debe retornar el promedio de edad de los amigos de una persona
-    // Ej:
-    // Si la persona tuviera estos amigos:
-    // {
-    //   amigos: [{
-    //     nombre: 'toni',
-    //     edad: 33,
-    //   }, {
-    //     nombre: 'Emi',
-    //     edad: 25
-    //   }]
-    // }
-    // persona.getPromedioEdad() debería devolver 29 ya que (33 + 25) / 2 = 29
-
-    // Tu código aca:
-
+        var cantidadAmigos = this.amigos.length;
+        var sumaEdades = 0;
+        for (var i = 0; i < this.amigos.length; i++) {
+            sumaEdades += this.amigos[i].edad;
+        }
+        var promedioEdades = sumaEdades / cantidadAmigos;
+        console.log(promedioEdades);
     }
 };
 
@@ -71,6 +53,10 @@ carlos.addFriend("Franco", 45);
 carlos.addFriend("Ana", 41);
 
 carlos.addHobby("cocinar");
+carlos.addHobby("viajar");
+carlos.addHobby("escribir");
 
 carlos.getFriends();
-//console.log(carlos);
+carlos.getHobbies();
+
+carlos.getPromedioEdad();
