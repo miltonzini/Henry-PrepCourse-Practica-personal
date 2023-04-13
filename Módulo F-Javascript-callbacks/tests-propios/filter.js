607daba98ec5c92f1e39ee761aa804f3240cdae7
miltonzini <miltonzini@gmail.com>
2023-04-12 23:47:38 -0300

    work on JS exercise variation (filter)

--- Módulo F-Javascript-callbacks/tests-propios/filter.js
+++ Módulo F-Javascript-callbacks/tests-propios/filter.js
@@ -3,24 +3,51 @@
 //  que los quite, devolviendo una "stringCorregida". 
 // Luego hará el filtrado de aquellos que empiecen con la letra "c".
 
-function checkString() {
+function checkString(string) {
    // temp
-   console.log("pasó por checkString");
+   //console.log("pasó por checkString");
+   var regex = /^[a-zA-Z]+$/; // all chars are letters
+   if (regex.test(string)) {
+      return true;
+   } else {
+      console.log(string + " contiene otros caracteres"); // temp
+      return false;
+   }   
 }
 
-function correctString() {
-   // temp
-   console.log("pasó por correctString");
+function correctString(string) { // SEGUIR TRABAJANDO
+   var tempArray = string.split('');
+   var correctedArray = [];
+   for (let i = 0; i < tempArray.length; i++) {
+      var regex = /[a-zaA-Z]/;
+      if (regex.test(tempArray[i])) { // 
+         correctedArray.push(tempArray[i]);
+      } 
+   };
+   
+   var correctedString = correctedArray.join('');
+   return correctedString
+   
 }
 
 function correctAndFilter(arrayOfStrings, cbCheckString, cbCorrecString) {
     var arrayFinal = [];
- 
-    for (let i = 0; i < arrayOfStrings.length; i++) {
-       if (arrayOfStrings[i][0] === "c") {
-         arrayFinal.push(arrayOfStrings[i]);
-       }
-    }
+
+   for (let i = 0; i < arrayOfStrings.length; i++) {
+      var isCorrect = checkString(arrayOfStrings[i]);
+      var string = "";
+      if (isCorrect === false) {
+         string = correctString(arrayOfStrings[i]);
+         console.log(string);
+      }
+   
+   }
+   // console.log("test asd");
+   // for (let i = 0; i < arrayOfStrings.length; i++) {
+   //    if (arrayOfStrings[i][0] === "c") {
+   //    arrayFinal.push(arrayOfStrings[i]);
+   //    }
+   // }
 
     console.log(arrayFinal);
  }
